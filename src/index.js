26be7dc5d3600c7dcebc461392c81fcd10fa5efb
Diego Haro Vivanco <diego.harovivanco@gmail.com>
2023-07-06 11:26:57 -0300

    upgrade index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ const app = require('./app.js')
 
 app.listen(app.get('port'), ()=>{
     console.log("Servidor escuchando en el puerto", app.get("port"))
+    console.log('\x1b[34m%s\x1b[0m', 'Acceder desde [Ctrl + Clic ] ==>','\x1b[32m\x1b[0m',` http://localhost:${app.get("port")} \r`);
 })
